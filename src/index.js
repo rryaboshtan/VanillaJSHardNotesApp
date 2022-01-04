a44e4d3c13949d0b7f961676761a7b9a2613ecdc
rryaboshtan <rrvworkroman@gmail.com>
2022-01-05 01:58:51 +0200

    note edit is working

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const todos = [
       command: 'sdfd',
    },
    {
-      name: 'The teory of evolution',
+      name: 'The theory of evolution',
       created: new Date().toLocaleDateString('en-US', options),
       category: 'Random Thought',
       content: 'The evolution...',
@@ -62,7 +62,7 @@ function renderTodoList(todos) {
                <button><i class="fas fa-archive"></i></button>
                <button><i class="fas fa-trash"></i></button></td>`;
          } else {
-            str += `<td><input type="text" disabled value="${todo[todoField]}"></td>`;
+            str += `<td><input data-field="${todoField}" type="text" disabled value="${todo[todoField]}"></td>`;
          }
       }
       str += `</tr>`;
@@ -72,3 +72,37 @@ function renderTodoList(todos) {
 }
 
 renderTodoList(todos);
+addEvents();
+
+function addEvents() {
+   let isEditMode = false;
+
+   const editIcons = document.querySelectorAll('.fa-pencil-alt');
+   const editButtons = Array.from(editIcons).map(iconElement => iconElement.parentElement);
+   editButtons.forEach((editButton, index) => {
+      editButton.addEventListener('click', () => {
+         isEditMode = !isEditMode;
+         console.log('sdfsdfsdf');
+         const tbody = document.querySelector('.table-body');
+         const currentRow = tbody.children[index];
+
+         const inputs = currentRow.querySelectorAll('input');
+
+         inputs.forEach(input => {
+            if (!isEditMode) {
+               input.setAttribute('disabled', 'disabled');
+               input.style.backgroundColor = '#ced7e488';
+               return
+            }
+            input.removeAttribute('disabled');
+            input.style.backgroundColor = '#ffffff';
+
+            input.addEventListener('change', event => {
+               todos[index][input.dataset.field] = event.target.value;
+               console.log(todos);
+            });
+         });
+      });
+   });
+   console.log(editButtons);
+}
