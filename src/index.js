b062d924da8caeb469d72d74bd9df0972e01dcdc
rryaboshtan <rrvworkroman@gmail.com>
2022-01-05 01:01:20 +0200

    create markup

--- src/index.js
+++ src/index.js
@@ -42,10 +42,10 @@ const todos = [
    },
 ];
 const categories = {
-   Task: '<div class="round-fone"><i class="fas fa-shopping-cart head"></i></div>',
-   Idea: '<div class="round-fone"><i class="far fa-lightbulb head"></i></div>',
-   'Random Thought': '<div class="round-fone"><i class="fas fa-user-md head"></i></div>',
-   Quote: '<div class="round-fone"><i class="fas fa-quote-right head"></i></div>',
+   Task: '<div class="round-fon"><i class="fas fa-shopping-cart head"></i></div>',
+   Idea: '<div class="round-fon"><i class="far fa-lightbulb head"></i></div>',
+   'Random Thought': '<div class="round-fon"><i class="fas fa-user-md head"></i></div>',
+   Quote: '<div class="round-fon"><i class="fas fa-quote-right head"></i></div>',
 };
 
 console.log(todos);
