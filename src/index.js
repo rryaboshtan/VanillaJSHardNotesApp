d6c62ee01d63faf0658ac1ba2266b5b2d4486aa7
rryaboshtan <rrvworkroman@gmail.com>
2022-01-05 05:16:32 +0200

    Fix bug and Add Note feature

--- src/index.js
+++ src/index.js
@@ -74,6 +74,47 @@ function renderTodoList(todos) {
 renderTodoList(todos);
 addEditEvents();
 addDeleteEvents();
+addCreateNoteEvent();
+function renderNewRow() {
+   const tbody = document.querySelector('.table-body');
+   const todoFields = Object.keys(todos[0]);
+   const content = 'Some data' + parseInt(Math.random() * 200);
+
+   todos.push({
+      name: '',
+      created: new Date().toLocaleDateString('en-US', options),
+      category: todos[0].category,
+      content,
+      dates: '',
+      command: '',
+   });
+   const addedTodo = todos[todos.length - 1];
+
+   let str = `<tr data-content="${content}"><td class="first-column">${categories[addedTodo.category]}</td>`;
+   for (let todoField of todoFields) {
+      if (todoField === 'command') {
+         str += `<td class="command"><button><i class="fas fa-pencil-alt"></i></button>
+               <button><i class="fas fa-archive"></i></button>
+               <button><i class="fas fa-trash"></i></button></td>`;
+      } else {
+         // let value = todoField === 'category' ? todos[0].category : ''
+         // let value = todoField === 'content' ? content : todoField === 'category' ? todos[0].category : '';
+         str += `<td><input data-field="${todoField}" type="text" disabled value="${addedTodo[todoField]}"></td>`;
+      }
+   }
+   str += `</tr>`;
+   tbody.innerHTML += str;
+
+   addEditEvents();
+   addDeleteEvents();
+}
+
+function addCreateNoteEvent() {
+   const createNoteButton = document.querySelector('.create-note');
+   createNoteButton.addEventListener('click', () => {
+      renderNewRow();
+   });
+}
 
 function addDeleteEvents() {
    let deleteIcons = document.querySelectorAll('.fa-trash');
@@ -87,7 +128,7 @@ function addDeleteEvents() {
 
          const currentTodoIndex = todos.findIndex(todo => todo.content === currentRow.dataset.content);
          todos.splice(currentTodoIndex, 1);
-
+         console.log(todos);
          currentRow.remove();
       });
    });
@@ -95,11 +136,12 @@ function addDeleteEvents() {
 
 function addEditEvents() {
    let isEditMode = false;
+   let oldValue = null;
 
    const editIcons = document.querySelectorAll('.fa-pencil-alt');
    const editButtons = Array.from(editIcons).map(iconElement => iconElement.parentElement);
    editButtons.forEach((editButton, index) => {
-      editButton.addEventListener('click', (event) => {
+      editButton.addEventListener('click', event => {
          isEditMode = !isEditMode;
          const tbody = document.querySelector('.table-body');
          const currentRow = event.target.parentElement.parentElement.parentElement;
@@ -115,10 +157,22 @@ function addEditEvents() {
             input.removeAttribute('disabled');
             input.style.backgroundColor = '#ffffff';
 
+            input.addEventListener('click', event => {
+               if (event.target.dataset.field === 'content') {
+                  oldValue = event.target.value;
+               }
+            });
             input.addEventListener('change', event => {
-                const currentTodoIndex = todos.findIndex(todo => todo.content === currentRow.dataset.content);
+               const currentRow = event.target.parentElement.parentElement;
+               // currentRow.dataset.content = oldValue;
+               console.log(currentRow);
+               const currentTodoIndex = todos.findIndex(todo => todo.content === currentRow.dataset.content);
                //  todos.splice(currentTodoIndex, 1);
                todos[currentTodoIndex][input.dataset.field] = event.target.value;
+               if (input.dataset.field === 'content') {
+                  currentRow.dataset.content = event.target.value;
+               }
+               // oldValue = event.target.value;
                console.log(todos);
             });
          });
